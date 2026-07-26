3996aabff6419e2652f41836fa843ef73033f836
aed474 <ismaildarnouni15@gmail.com>
2026-07-26 14:45:14 +0100

    [ADD] convex backend Setup & integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import {ConvexClientProvider} from "@/components/ConvexClientProvider";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
@@ -28,7 +28,9 @@ export default function RootLayout({
                 inter.variable
             )}
         >
-        {children}
+        <ConvexClientProvider>
+            {children}
+        </ConvexClientProvider>
         </body>
         </html>
     );
